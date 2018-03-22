f3bf6984583ec8a4dc624a6d9d6ddf9d64e38c42
Andrii Afanasiev <andrii@crytek.de>
2018-03-22 15:49:47 +0100

    return back a hack with phpunit versions

--- Test/bootstrap.php
+++ Test/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
 
 include __DIR__ . "/../vendor/autoload.php";
+
+// backward compatibility (https://stackoverflow.com/a/42828632/187780)
+if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
