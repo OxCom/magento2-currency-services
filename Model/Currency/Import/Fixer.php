d16ba740121d7fcaa1198ec22b7abfd571dce105
OxCom <gm.oxcom@gmail.com>
2018-06-16 15:21:16 +0200

    update: fixer API endpoint and rate hack for free token

--- Model/Currency/Import/Fixer.php
+++ Model/Currency/Import/Fixer.php
@@ -11,8 +11,9 @@ use OxCom\MagentoCurrencyServices\Model\Currency\Import\Fixer\Rates;
  */
 class Fixer extends AbstractSource
 {
-    const SOURCE_NAME = 'fixer';
-    const SOURCE_LINK = 'http://api.fixer.io/latest?access_key={{TOKEN}}&base={{FROM}}&symbols={{TO}}';
+    const SOURCE_NAME      = 'fixer';
+    const SOURCE_LINK      = 'http://data.fixer.io/api/latest?access_key={{TOKEN}}&base={{FROM}}&symbols={{TO}}';
+    const SOURCE_LINK_FREE = 'http://data.fixer.io/api/latest?access_key={{TOKEN}}';
 
     /**
      * Retrieve rate
@@ -37,11 +38,56 @@ class Fixer extends AbstractSource
         ]);
 
         try {
+            // We are not able to detect typ of token (free, paid), so we have to do 2 API calls:
+            // 1. Request as paid token
+            // 2. Request as free token
             $response = $this->request($url);
             $data     = json_decode($response);
             $rates    = new Rates($data);
 
-            $rate = $rates->getRates($currencyTo);
+            if (!empty($data) && empty($data->success)) {
+                // example: {"success":false,"error":{"code":105,"type":"base_currency_access_restricted"}}
+                $url  = strtr(static::SOURCE_LINK_FREE, [
+                    '{{TOKEN}}' => $this->getAccessToken(),
+                ]);
+
+                $response = $this->request($url);
+                $data     = json_decode($response);
+                $rates    = new Rates($data);
+
+                switch (true) {
+                    case ($currencyFrom === $rates->getBase()):
+                        // this is general case
+                        $rate = $rates->getRates($currencyTo);
+                        break;
+
+                    case ($currencyTo === $rates->getBase()):
+                        $rate = $rates->getRates($currencyFrom);
+
+                        if (empty($rate)) {
+                            throw new \Exception();
+                        }
+
+                        $rate = 1 / $rate;
+                        break;
+
+                    default:
+                        // small trick: we can convert from {FROM} to {TO} with intermediate currency
+                        // 1 EUR = a * 1 {FROM} and 1 EUR = b * 1 {TO}
+                        // so 1 {FROM} = b * 1 {TO} / a
+                        $baseFromRate = $rates->getRates($currencyFrom);
+                        $baseToRate   = $rates->getRates($currencyTo);
+
+                        if (empty($baseFromRate) || $baseToRate) {
+                            throw new \Exception();
+                        }
+
+                        $rate = ($baseToRate / $baseFromRate);
+                }
+            } else {
+                $rate = $rates->getRates($currencyTo);
+            }
+
 
             if (empty($rate)) {
                 throw new \Exception();
